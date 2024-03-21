42141cb6a0b44de09aa313f99f97c52c806bcd02
Pedro Mota <pedrodaluzmota208@gmail.com>
2024-03-21 20:19:46 -0300

    :sparkles: feat: criando classe de atendimento

---
+++ AtividadePratica1/src/Atendimento.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Atendimento {
+    private String nomeProfessor;
+    private String horarioAtendimento;
+    private String periodo;
+    private int sala;
+    private ArrayList<Integer> predio;
+
+/*
+1: 1 - 5
+2: 6 - 10
+3: 11 - 15
+4: 16 - 20
+5: 21 - 25
+6: 26 - 30
+ */
+
+    public Atendimento(String nomeProfessor, String horarioAtendimento, String periodo, int sala,
+            ArrayList<Integer> predio) {
+        this.nomeProfessor = nomeProfessor;
+        this.horarioAtendimento = horarioAtendimento;
+        this.periodo = periodo;
+        this.sala = sala;
+        this.predio = predio;
+    }
+
+    int predioAtendimento(int sala){
+        int predio = 0;
+
+        if(sala >= 26 && sala <= 30){
+            predio = 6;
+        }else if(sala >= 21){
+            predio = 5;
+        }else if(sala >= 16){
+            predio = 4;
+        }else if(sala >= 11){
+            predio = 3;
+        }else if(sala >= 6){
+            predio = 2;
+        }else if(sala >= 1){
+            predio = 1;
+        }
+        
+        return predio;
+    }
+}
